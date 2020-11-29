ca73f019b3d1ce424638d8195dcec98f622f4d0f
Alvi Jawad <alvijawad3325@gmail.com>
2020-11-29 09:03:18 -0500

    Added a helper function

---
+++ helper.h
@@ -0,0 +1,11 @@
+#ifndef __HELPER_H__
+#define __HELPER_H__
+
+/**
+ * This header file contains some helper functions
+ * bool objectHasTag(): whether user input <arg> matches any OBJECT tag
+ */
+extern int list_objects_at_location(OBJECT_t *location);
+
+
+#endif
